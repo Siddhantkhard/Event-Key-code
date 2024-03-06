4d15812aa6427dcd2d063e4848976f188b67f18f
Siddhantkhard <160150596+Siddhantkhard@users.noreply.github.com>
2024-03-06 18:33:31 +0530

    Create style.js

---
+++ style.js
@@ -0,0 +1,40 @@
+*{
+    box-sizing: border-box;
+}
+
+body{
+    background-color: rgb(70, 70, 70);
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    height: 100vh;
+    overflow: hidden;
+    margin: 0px;
+
+}
+
+.key{
+    border: 1px greenyellow;
+    background-color: beige;
+    display: inline-flex;
+    align-items: center;
+    font-size: 20px;
+    font-weight: bold;
+    padding: 20px;
+    min-width: 150px;
+    flex-direction: column;
+    margin: 10px;
+    color: rgb(10, 11, 11);
+    position: relative;
+}
+
+
+
+
+
+
+
+
+
+
